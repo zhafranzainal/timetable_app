645b661c24a389515f2e1a967ec6861a248cc709
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-01 02:47:36 +0800

    Organize screens into folder

---
+++ lib/model/course_model.dart
@@ -0,0 +1,17 @@
+class CourseModel {
+  String code;
+  String name;
+  String section;
+  String lab;
+  String time;
+  String location;
+
+  CourseModel({
+    required this.code,
+    required this.name,
+    required this.section,
+    required this.lab,
+    required this.time,
+    required this.location,
+  });
+}
---
+++ lib/screens/home_page_screen.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:timetable_app/model/course_model.dart';
+
+class HomePageScreen extends StatefulWidget {
+  const HomePageScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePageScreen> createState() => _HomePageScreenState();
+}
+
+class _HomePageScreenState extends State<HomePageScreen> {
+  String searchedCourse = '';
+  String selectedCourseCode = '';
+  String selectedCourseName = '';
+  String selectedSection = '';
+  String selectedLab = '';
+
+  // Course code to name mapping
+  List<String> courseCodes = ['BCS1033', 'BCI2023'];
+  Map<String, String> courseMap = {
+    'BCS1033': 'SOFTWARE ENGINEERING',
+    'BCI2023': 'DATABASE SYSTEMS',
+  };
+
+  List<CourseModel> timetable = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Search Course
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  searchedCourse = value;
+                });
+              },
+              decoration: const InputDecoration(
+                labelText: 'Search Course',
+              ),
+            ),
+            // Show matched course codes and names as dropdown
+            searchedCourse.isNotEmpty
+                ? Expanded(
+                    child: Dialog(
+                      child: ListView.builder(
+                        itemCount: courseCodes.length,
+                        itemBuilder: (context, index) {
+                          String courseCode = courseCodes[index];
+                          String courseName = courseMap[courseCode] ?? '';
+                          if (courseCode
+                                  .toLowerCase()
+                                  .contains(searchedCourse.toLowerCase()) ||
+                              courseName
+                                  .toLowerCase()
+                                  .contains(searchedCourse.toLowerCase())) {
+                            return ListTile(
+                              title: Text('$courseCode $courseName'),
+                              onTap: () {
+                                setState(() {
+                                  selectedCourseCode = courseCode;
+                                  selectedCourseName = courseName;
+                                });
+                                Navigator.of(context).pop();
+                              },
+                            );
+                          } else {
+                            return const SizedBox.shrink();
+                          }
+                        },
+                      ),
+                    ),
+                  )
+                : const SizedBox.shrink(),
+            // Add Section
+            DropdownButton<String>(
+              value: selectedSection.isNotEmpty
+                  ? selectedSection
+                  : 'Select Section',
+              onChanged: (value) {
+                setState(() {
+                  selectedSection = value!;
+                });
+              },
+              items: <String>['Select Section', '01', '02', '03']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            // Add Lab
+            DropdownButton<String>(
+              value: selectedLab.isNotEmpty ? selectedLab : 'Select Lab',
+              onChanged: (value) {
+                setState(() {
+                  selectedLab = value!;
+                });
+              },
+              items: <String>['Select Lab', '01A', '01B']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            // Add Button to Generate Timetable
+            ElevatedButton(
+              onPressed: () {
+                generateTimetable();
+              },
+              child: const Text('Generate Timetable'),
+            ),
+            // Add List View to display timetable
+            Expanded(
+              child: ListView.builder(
+                itemCount: timetable.length,
+                itemBuilder: (context, index) {
+                  CourseModel course = timetable[index];
+                  return ListTile(
+                    title: Text('Course Code: ${course.code}'),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Course Name: ${course.name}'),
+                        Text('Section: ${course.section}'),
+                        Text('Lab: ${course.lab}'),
+                        Text('Time: ${course.time}'),
+                        Text('Location: ${course.location}'),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void generateTimetable() {
+    setState(() {
+      timetable = [
+        CourseModel(
+            code: selectedCourseCode,
+            name: selectedCourseName,
+            section: selectedSection,
+            lab: selectedLab,
+            time: '8:00 AM - 10:00 AM',
+            location: 'FSK15'),
+        CourseModel(
+            code: selectedCourseCode,
+            name: selectedCourseName,
+            section: selectedSection,
+            lab: selectedLab,
+            time: '10:30 AM - 12:30 PM',
+            location: 'FSK15'),
+      ];
+    });
+  }
+}
