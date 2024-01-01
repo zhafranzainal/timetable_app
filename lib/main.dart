391d560a9e333c6987a63f6095596c537ce30cbb
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-02 02:26:00 +0800

    Add calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:timetable_app/screens/home_page_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  initializeDateFormatting().then((_) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:timetable_app/public_components/sidebar_menu.dart';
 
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});
@@ -8,8 +10,51 @@ class CalendarScreen extends StatefulWidget {
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
+  DateTime today = DateTime.now();
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      drawer: const SidebarMenu(),
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade700,
+        title: const Text('Calendar', style: TextStyle(color: Colors.white)),
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
+      ),
+      body: content(),
+    );
+  }
+
+  Widget content() {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Text('Selected Day = ' + today.toString().split(" ")[0]),
+          Container(
+            child: TableCalendar(
+              headerStyle: const HeaderStyle(
+                formatButtonVisible: false,
+                titleCentered: true,
+              ),
+              locale: 'ms_MY',
+              availableGestures: AvailableGestures.all,
+              selectedDayPredicate: (day) => isSameDay(day, today),
+              focusedDay: today,
+              firstDay: DateTime.utc(2023, 1, 1),
+              lastDay: DateTime.utc(2025, 12, 31),
+              onDaySelected: _onDaySelected,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
