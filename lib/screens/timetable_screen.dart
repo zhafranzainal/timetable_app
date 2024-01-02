8b65c0be6c3eb52c76cc6ac80bdf52ca5868be85
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-03 03:46:01 +0800

    Add timetable layout

--- lib/screens/timetable_screen.dart
+++ lib/screens/timetable_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timetable_view/timetable_view.dart';
 
 class TimetableScreen extends StatefulWidget {
   const TimetableScreen({super.key});
@@ -17,5 +18,55 @@ class _TimetableScreenState extends State<TimetableScreen> {
             color: Colors.white,
           ),
         ),
+        body: TimetableView(
+          laneEventsList: _buildLaneEvents(),
+          onEventTap: onEventTapCallBack,
+          timetableStyle: const TimetableStyle(),
+          onEmptySlotTap: onTimeSlotTappedCallBack,
+        ),
       );
+
+  List<LaneEvents> _buildLaneEvents() {
+    return [
+      LaneEvents(
+        lane: Lane(name: 'Monday', laneIndex: 1),
+        events: [],
+      ),
+      LaneEvents(
+        lane: Lane(name: 'Tuesday', laneIndex: 2),
+        events: [],
+      ),
+      LaneEvents(
+        lane: Lane(name: 'Wednesday', laneIndex: 3),
+        events: [],
+      ),
+      LaneEvents(
+        lane: Lane(name: 'Thursday', laneIndex: 4),
+        events: [],
+      ),
+      LaneEvents(
+        lane: Lane(name: 'Friday', laneIndex: 5),
+        events: [],
+      ),
+      LaneEvents(
+        lane: Lane(name: 'Saturday', laneIndex: 6),
+        events: [],
+      ),
+      LaneEvents(
+        lane: Lane(name: 'Sunday', laneIndex: 7),
+        events: [],
+      ),
+    ];
+  }
+
+  void onEventTapCallBack(TableEvent event) {
+    print(
+        "Event Clicked!! LaneIndex ${event.laneIndex} Title: ${event.title} StartHour: ${event.startTime.hour} EndHour: ${event.endTime.hour}");
+  }
+
+  void onTimeSlotTappedCallBack(
+      int laneIndex, TableEventTime start, TableEventTime end) {
+    print(
+        "Empty Slot Clicked !! LaneIndex: $laneIndex StartHour: ${start.hour} EndHour: ${end.hour}");
+  }
 }
