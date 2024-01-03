3e5ff64bc4c9eef82b0480738a868ed1b5a31d40
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-03 15:25:18 +0800

    Add homepage screen with logo

--- lib/screens/home_page_screen.dart
+++ lib/screens/home_page_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:timetable_app/public_components/sidebar_menu.dart';
+import 'package:timetable_app/screens/create_timetable_screen.dart';
 
 class HomePageScreen extends StatelessWidget {
   const HomePageScreen({super.key});
@@ -13,6 +14,21 @@ class HomePageScreen extends StatelessWidget {
           iconTheme: const IconThemeData(
             color: Colors.white,
           ),
+        ),
+        body: Center(
+          child: Image.asset(
+            'assets/images/logo_umpsa.png',
+            width: 200,
+            height: 200,
+            fit: BoxFit.contain,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const CreateTimetableScreen()));
+          },
         ),
       );
 }
